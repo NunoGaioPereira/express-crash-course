3cc053801b2f03ee5057601ca90e948a614c924d
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-11 23:47:01 +0100

    feat: api to get individual member

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const logger = require('./middleware/logger');
 const app = express();
 
 // Init middleware
-app.use(logger);
+// app.use(logger);
 
 // Gets all members
 app.get('/api/members', (req, res) => res.json(members));
 
+// Get single member
+app.get('/api/members/:id', (req, res) => {
+	// res.send(req.params.id);
+	const found = members.some(member => member.id === parseInt(req.params.id));
+	if (found) {
+		res.json(members.filter(member => member.id === parseInt(req.params.id)));
+	}
+	else {
+		res.status(400).json({ msg: 'Member not found with id of ${req.params.id}' });
+	}
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
