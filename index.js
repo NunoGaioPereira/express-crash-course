7a340c6fcf6bc93d8e63765772da8353dfbe7484
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-11 23:22:43 +0100

    references

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 const express = require('express');
+const path = require('path');
 
 // Init express
 const app = express();
+
+// Route
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bob@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannon@gmail.com',
+    status: 'active'
+  }
+];
+
+app.get('/api/members', (req, res) => {
+
+});
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+// Run server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
