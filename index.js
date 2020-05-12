69dac64c7026cbbdd2d20c9ae931eb0f60b7a494
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-12 22:34:04 +0100

    feat: create views rendering

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 // Init express
 const app = express();
 
 // Init middleware
 // app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Hopemage route
+app.get('/', (req, res) => res.render('index', {
+	title: 'Member App',
+	members
+	// members: members
+}));
 
 // Body parser middleware
 app.use(express.json());
